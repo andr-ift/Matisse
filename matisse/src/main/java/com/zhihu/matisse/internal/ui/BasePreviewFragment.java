a2dbcc1e2399d8f0ab97694bb70ced0f575d239c
andr-ift <35932862+andr-ift@users.noreply.github.com>
2019-10-04 17:32:28 +0300

    Androidx fixes;

--- matisse/src/main/java/com/zhihu/matisse/internal/ui/BasePreviewFragment.java
+++ matisse/src/main/java/com/zhihu/matisse/internal/ui/BasePreviewFragment.java
@@ -1,23 +1,20 @@
 package com.zhihu.matisse.internal.ui;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.zhihu.matisse.Matisse;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
 import com.zhihu.matisse.R;
-import com.zhihu.matisse.internal.entity.IncapableCause;
 import com.zhihu.matisse.internal.entity.Item;
 import com.zhihu.matisse.internal.entity.SelectionSpec;
 import com.zhihu.matisse.internal.model.SelectedItemCollection;
--- matisse/src/main/java/com/zhihu/matisse/internal/ui/MediaSelectionFragment.java
+++ matisse/src/main/java/com/zhihu/matisse/internal/ui/MediaSelectionFragment.java
@@ -20,6 +20,7 @@ import android.database.Cursor;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
